726f8ac428fdea1a15105d1dd6960e179f23af42
Simon Jäger <simon-jaeger@users.noreply.github.com>
2025-01-16 19:00:31 +0100

    j

--- _intro/headless/sum.php
+++ _intro/headless/sum.php
@@ -3,10 +3,12 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
-$a = $data['a'];
-$b = $data['b'];
+$json = file_get_contents('php://input'); // {"a": 2, "b:': 4 }
+$data = json_decode($json, true); // [ "a" => 2, "b" => 4]
+$a = $data['a']; // 2
+$b = $data['b']; // 4
 
-$data = ['sum' => $a + $b];
+$sum = $a + $b;
+
+$data = ['sum' => $sum];
 echo json_encode($data);

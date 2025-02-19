c2175bbadcfd593ffa9952354b765fef03533daa
Simon Jäger <simon-jaeger@users.noreply.github.com>
2025-02-19 19:49:06 +0100

    j

--- database/migrations/040_tags.php
+++ database/migrations/040_tags.php
@@ -6,19 +6,11 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
   function up() {
-    // main table
     Schema::create('tags', function (Blueprint $table) {
       $table->id();
       $table->string('name');
       $table->timestamp('created_at');
       $table->timestamp('updated_at');
-    });
-
-    // pivot table
-    Schema::create('article_tag', function (Blueprint $table) {
-      $table->id();
-      $table->foreignId('article_id')->constrained()->cascadeOnDelete();
-      $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
     });
   }
 

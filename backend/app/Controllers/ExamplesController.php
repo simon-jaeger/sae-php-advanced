7f7ad9669b66758a17e7fb26fc216f49823516fe
Simon Jäger <simon-jaeger@users.noreply.github.com>
2025-07-08 20:34:01 +0200

    j

--- backend/app/Controllers/ExamplesController.php
+++ backend/app/Controllers/ExamplesController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class ExamplesController {
   function ping() {
@@ -20,12 +21,71 @@ class ExamplesController {
   }
 
   function random(Request $request) {
-    $array = ['a','b','c'];
+    $array = ['a', 'b', 'c'];
     Arr::random($array);
   }
 
   function echo(Request $request) {
-    $text = $request->input('text'); // $_POST['text']
+    $text = $request->input('text'); // similar to $_POST['text']
     return $text;
+  }
+
+  function reverse(Request $request) {
+    $text = $request->input('text');
+    return Str::reverse($text); // handles unicode like umlauts better than strrev()
+  }
+
+  function sum(Request $request) {
+    $numbers = $request->input('numbers');
+    return array_sum($numbers);
+  }
+
+  function count(Request $request) {
+    $text = $request->input('text');
+    return str_word_count($text);
+  }
+
+  function palindrom(Request $request) {
+    $text = $request->input('text');
+    return Str::reverse($text) === $text ? 'yes' : 'no';
+  }
+
+  function anagram(Request $request) {
+    $a = $request->input('a');
+    $b = $request->input('b');
+    $normalize = function ($s) {
+      $s = str_split($s);
+      sort($s);
+      return implode($s);
+    };
+    return $normalize($a) === $normalize($b) ? 'yes' : 'no';
+  }
+
+  function temperature(Request $request) {
+    $celsius = $request->input('celsius');
+    return [
+      'celsius' => $celsius,
+      'kelvin' => $celsius + 273.15,
+      'fahrenheit' => ($celsius * 9 / 5) + 32,
+    ];
+  }
+
+  function caesar(Request $request) {
+    $text = $request->input('text');
+    $shift = $request->input('shift');
+
+    $alphabet = range('a', 'z');
+    $encrypted = '';
+
+    foreach (str_split($text) as $char) {
+      $from = array_search($char, $alphabet);
+      $to = ($from + $shift) % 26;
+      $encrypted .= $alphabet[$to];
+    }
+
+    return [
+      'original' => $text,
+      'encrypted' => $encrypted,
+    ];
   }
 }

a69e991703cca99a54fbace24347fd7bb878c41a
Simon Jäger <simon-jaeger@users.noreply.github.com>
2025-07-29 20:09:50 +0200

    j

--- backend/app/Controllers/ArticlesController.php
+++ backend/app/Controllers/ArticlesController.php
@@ -5,10 +5,10 @@ namespace App\Controllers;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class ArticlesController {
   function index(Request $request) {
-
     $id = $request->input('id');
     $userId = $request->input('user_id');
     $title = $request->input('title');
@@ -22,12 +22,38 @@ class ArticlesController {
     if ($id) return $query->where('id', $id)->firstOrFail();
     if ($userId) $query->where('user_id', $userId);
     if ($title) $query->where('title', 'like', "%$title%");
+    if ($tagIds) {
+      $tagIds = explode(',', $tagIds);
+      // $query->has('tags'); // articles that have tags
+      // $query->has('tags', '>=', count($tagIds)); // articles that have at least that many tags
+      $query->whereHas(
+        'tags',
+        fn($q) => $q->whereIn('tags.id', $tagIds), // only tags with ids that are part of the search array
+        '>=',
+        count($tagIds)
+      );
+    }
+
     $query->orderBy($orderBy, $orderDir);
     $query->limit($limit);
     $query->offset($offset);
 
     // return $query->toSql(); // for debugging
     return $query->get();
+  }
+
+  function search(Request $request) {
+    $title = $request->input('title');
+    return Article::select('id', 'title')
+      ->get()
+      ->map(fn($a) => [
+        'id' => $a->id,
+        'title' => $a->title,
+        'distance' => levenshtein($title, $a->title),
+        'similarity' => similar_text($title, $a->title),
+      ])
+      ->sortBy('distance')
+      ->values();
   }
 
   function create(Request $request) {

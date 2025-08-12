4396cd5b344d807a1906c30c66c629d06dcd0301
Simon Jäger <simon-jaeger@users.noreply.github.com>
2025-08-12 19:06:00 +0200

    j

--- backend/app/Controllers/AiController.php
+++ backend/app/Controllers/AiController.php
@@ -10,9 +10,9 @@ use Illuminate\Support\Facades\Http;
 class AiController {
   function prompt(Request $request) {
     $prompt = $request->input('prompt');
-    $response = Http::post("http://localhost:11434/api/generate", [
-      "model" => "gemma3:1b",
-      "prompt" => $prompt,
+    $response = Http::post('http://localhost:11434/api/generate', [
+      'model' => 'gemma3:1b',
+      'prompt' => $prompt,
       'stream' => false,
     ]);
     $data = $response->json();
